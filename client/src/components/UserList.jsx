4ee449e165ee8c2407b59c41bf37f4f0485a1c51
AU5TRA <113289565+AU5TRA@users.noreply.github.com>
2024-01-09 15:01:50 +0600

    userlist shown

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const UserList = () => {
+    const [users, setUsers] = useState([]);
+
+    const getUser = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/users');
+            const jsonData = await response.json();
+            if (jsonData && jsonData.data && Array.isArray(jsonData.data.passengers))
+                setUsers(jsonData.data.passengers);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
     return (
         <div className='list-group'>
             <table className="table table-hover table-dark">
@@ -12,39 +29,23 @@ const UserList = () => {
                         <th scope="col">Gender</th>
                         <th scope="col">Edit</th>
                         <th scope="col">Delete</th>
-
                     </tr>
                 </thead>
-
                 <tbody>
-                    <tr>
-                        <td>Ifti</td>
-                        <td>ifti@example.com</td>
-                        <td>12324234234</td>
-                        <td>F</td>
-                        <td> <button className="btn btn-warning">Update</button></td>
-                        <td> <button className="btn btn-danger">Delete</button></td>
-
-                    </tr>
-                    <tr>
-                        <td>Nisa</td>
-                        <td>nisa@example.com</td>
-                        <td>1872368721</td>
-                        <td>F</td>
-                        <td> <button className="btn btn-warning">Update</button></td>
-                        <td> <button className="btn btn-danger">Delete</button></td>
-
-
-                    </tr>
-                    
+                    {users.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{user.nid}</td>
+                            <td>{user.gender}</td>
+                            <td><button className="btn btn-warning">Update</button></td>
+                            <td><button className="btn btn-danger">Delete</button></td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
-
-
-
-
         </div>
-    )
-}
+    );
+};
 
-export default UserList
+export default UserList;
