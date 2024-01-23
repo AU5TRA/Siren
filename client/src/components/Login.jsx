7677219131809f5e067bcd35ea1605b424b0cc50
artsua <155561353+artsua@users.noreply.github.com>
2024-01-24 02:29:30 +0600

    no changes made

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,8 @@ import React, { Fragment, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { toast } from "react-toastify";
 import './login.css'
+//import './login2.css'
+
 
 const Login = ({ setAuth }) => {
 
@@ -25,21 +27,21 @@ const Login = ({ setAuth }) => {
             });
             const json = await response.json()
             console.log(json.message);
-            if(json.status === 200){
+            if (json.status === 200) {
                 console.log(json.data.res);
             }
-            
+
             setShowMessage(json.message);
-        
+
             if (json.jwtToken) {
                 localStorage.setItem("token", json.data.res);
                 setAuth(true);
                 toast.success("Logged in Successfully");
-              } else {
+            } else {
                 setAuth(false);
                 toast.error(json);
-              }
-              if (json.success) {
+            }
+            if (json.success) {
                 setModalOpen(true);
             }
 
@@ -49,7 +51,7 @@ const Login = ({ setAuth }) => {
         }
     }
 
-   
+
     const closeMessage = () => {
         setShowMessage(false);
         try {
@@ -68,44 +70,43 @@ const Login = ({ setAuth }) => {
             console.error(err.message);
         }
     }
-
     return <Fragment>
-        <div className="login-form-container">
-            <form onSubmit={handleSubmit} className="login-form">
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" value={email} onChange={e => setEmail(e.target.value)} id="email" name="email" aria-describedby="emailHelp" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} name="password" id="password" />
-                </div>
+    <div className="login-form-container">
+        <form onSubmit={handleSubmit} className="login-form">
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email address</label>
+                <input type="email" className="form-control" value={email} onChange={e => setEmail(e.target.value)} id="email" name="email" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} name="password" id="password" />
+            </div>
 
-                <button type="submit" className="btn btn-primary">Submit</button>
-                <p className="signup-message">Don't have an account? <button type="button" className="btn btn-secondary" onClick={registerUser}>Sign Up</button></p>
-            </form>
-        </div>
-        {showMessage && (
-            <div className="modal" style={{ display: 'block' }}>
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Login Status</h5>
-                            <button type="button" className="close" onClick={closeMessage}>
-                                <span>&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p>{showMessage}</p>
-                            <button type="button" className="btn btn-primary" onClick={closeMessage}>
-                                Close
-                            </button>
-                        </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <p className="signup-message">Don't have an account? <button type="button" className="btn btn-secondary" onClick={registerUser}>Sign Up</button></p>
+        </form>
+    </div>
+    {showMessage && (
+        <div className="modal" style={{ display: 'block' }}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Login Status</h5>
+                        <button type="button" className="close" onClick={closeMessage}>
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p>{showMessage}</p>
+                        <button type="button" className="btn btn-primary" onClick={closeMessage}>
+                            Close
+                        </button>
                     </div>
                 </div>
             </div>
-        )}
-    </Fragment>
+        </div>
+    )}
+</Fragment>
 }
 
 export default Login
