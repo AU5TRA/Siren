1d325c6f2fa51a616dae8753fa1cd987b808336d
Anika Morshed <anikamorshed4434@gmail.com>
2024-02-07 02:37:38 +0600

    # edit 1

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import './home.css';
+import './hp.css';
+// import './home.css';
 
 const HomePage = () => {
-    return (
-        <div>
-            <header className="header">
-                <img src="../../siren_home.png" style={{ width: '100px', height: '30px', marginLeft: '10px' }} />
+    // return (
+    //     <div>
+    //         <header className="header">
+    //             <img src="../../siren_home.png" style={{ width: '100px', height: '30px', marginLeft: '10px' }} />
 
-                <nav>
-                    <Link to="users/login">Login</Link>
-                    <Link to="/trains">Train Information</Link>
-                    <Link to="/booking/train/search">Book a SEAT!</Link>
-                </nav>
+    //             <nav>
+    //                 <Link to="users/login">Login</Link>
+    //                 <Link to="/trains">Train Information</Link>
+    //                 <Link to="/booking/train/search">Book a SEAT!</Link>
+    //             </nav>
+    //         </header>
+    //         <main className="main">
+    //             {/* Main content goes here */}
+    //         </main>
+    //         <footer className="footer">
+    //             {/* Footer content if any */}
+    //         </footer>
+    //     </div>
+    // );
+    return <Fragment>
+        <div className="sidebar">
+            <header className="header">
+                <div className="logo">
+                    <img src="../../siren_home.png" style={{ width: '150px', height: '50px', marginLeft: '10px' }} />
+                </div>
             </header>
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <Link to="users/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/trains">Train Information</Link>
+                    </li>
+                    <li>
+                        <Link to="/booking/train/search">Book a SEAT!</Link>
+                    </li>
+                </ul>
+            </nav>
             <main className="main">
                 {/* Main content goes here */}
             </main>
@@ -21,7 +50,7 @@ const HomePage = () => {
                 {/* Footer content if any */}
             </footer>
         </div>
-    );
+    </Fragment>
 };
 
 export default HomePage;
