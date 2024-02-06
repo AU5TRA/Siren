9d2eb62682a39491e0d379b5aa8c5b9fe6ce1012
artsua <155561353+artsua@users.noreply.github.com>
2024-02-07 02:33:28 +0600

    train routes fixed

--- client/src/components/TrainRoute.jsx
+++ client/src/components/TrainRoute.jsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
-
 function convertTo12HourFormat(time24) {
     const [hours, minutes] = time24.split(":");
     const parsedHours = parseInt(hours, 10);
-
     const period = parsedHours >= 12 ? "PM" : "AM";
-
     const hours12 = parsedHours % 12 || 12;
-
     return `${hours12}:${minutes} ${period}`;
 }
 
@@ -27,7 +23,6 @@ const TrainRoute = () => {
                 setRoute(rec.data.result);
                 const train_name = rec.data.result[0].train_name;
                 setTrainName(train_name); 
-                // console.log(userData);
             } catch (error) {
                 console.error(error.message);
             }
@@ -36,46 +31,73 @@ const TrainRoute = () => {
         fetchData();
     }, [id]);
 
+    const groupedRoutes = groupRoutesByRouteId(routes);
+
     return (
         <Fragment>
             <h3 style={{ marginBottom: '50px', marginTop:'50px'}}><b><center> {train_name} </center></b></h3>
-            {routes && routes.map(route => (
-                <div style={{ marginBottom: '30px' }}>
-                    
-                <ul className="list-group list-group-flush" style={{ marginBottom: '30px' }} key={route.station_id}>
-                    <li className="list-group-item">
-                        <img src="../../location.png" style={{ width: '20px', height: '25px' }} />
-                        <span style={{ marginLeft: '30px' }}></span>
-                        <b>{route.station_name}</b>
-                    </li>
-                    
-                    {route.arrival !== null && route.departure !== null && (
-                        <li className="list-group-item">
-                            Arrival: {convertTo12HourFormat(route.arrival)}
-                            <span style={{ marginLeft: '150px' }}> Departure: {convertTo12HourFormat(route.departure)}</span>
-                        </li>
-                    )}
-                    {route.arrival === null && route.departure !== null && (
-                        <li className="list-group-item">
-                            Departure: {convertTo12HourFormat(route.departure)}
-                        </li>
-                    )}
-                    {route.arrival !== null && route.departure === null && (
-                        <li className="list-group-item">
-                            Arrival: {convertTo12HourFormat(route.arrival)}
-                        </li>
-                    )}
-                    {route.arrival === null && route.departure === null && (
-                        <li className="list-group-item">
-                            No Arrival or Departure Information
-                        </li>
-                    )}
-                </ul>
+            {groupedRoutes.map((group, index) => (
+                <div key={index}>
+                    <h4>Route {index + 1}</h4>
+                    {group.map(route => (
+                        <div style={{ marginBottom: '30px' }} key={route.station_id}>
+                            <ul className="list-group list-group-flush" style={{ marginBottom: '30px' }}>
+                                <li className="list-group-item">
+                                    <img src="../../location.png" style={{ width: '20px', height: '25px' }} />
+                                    <span style={{ marginLeft: '30px' }}></span>
+                                    <b>{route.station_name}</b>
+                                </li>
+                                {route.arrival !== null && route.departure !== null && (
+                                    <li className="list-group-item">
+                                        Arrival: {convertTo12HourFormat(route.arrival)}
+                                        <span style={{ marginLeft: '150px' }}> Departure: {convertTo12HourFormat(route.departure)}</span>
+                                    </li>
+                                )}
+                                {route.arrival === null && route.departure !== null && (
+                                    <li className="list-group-item">
+                                        Departure: {convertTo12HourFormat(route.departure)}
+                                    </li>
+                                )}
+                                {route.arrival !== null && route.departure === null && (
+                                    <li className="list-group-item">
+                                        Arrival: {convertTo12HourFormat(route.arrival)}
+                                    </li>
+                                )}
+                                {route.arrival === null && route.departure === null && (
+                                    <li className="list-group-item">
+                                        No Arrival or Departure Information
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
             ))}
         </Fragment>
-
     );
+};
+
+const groupRoutesByRouteId = (routes) => {
+    const groupedRoutes = [];
+    let currentGroup = [];
+
+    if (routes) {
+        routes.forEach((route, index) => {
+            if (index === 0 || route.route_id !== routes[index - 1].route_id) {
+                if (currentGroup.length > 0) {
+                    groupedRoutes.push(currentGroup);
+                }
+                currentGroup = [route];
+            } else {
+                currentGroup.push(route);
+            }
+        });
+        if (currentGroup.length > 0) {
+            groupedRoutes.push(currentGroup);
+        }
+    }
+
+    return groupedRoutes;
 };
 
 export default TrainRoute;
