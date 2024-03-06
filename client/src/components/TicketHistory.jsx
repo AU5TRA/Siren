0ac43d3c4eb8a551b2c156555e72017acee24030
Anika Morshed <anikamorshed4434@gmail.com>
2024-03-06 12:03:04 +0600

    #slight change

--- client/src/components/TicketHistory.jsx
+++ client/src/components/TicketHistory.jsx
@@ -191,7 +191,7 @@ const TicketHistory = () => {
                                 <>
                                     {journey_map[transactionId].trainName},
                                     {journey_map[transactionId].className},
-                                    {journey_map[transactionId].status}
+                                    {formatDate(journey_map[transactionId].doj)}
                                     <span style={{ padding: '0 20px' }} className="spacer"></span>
                                 </>
                             ) : (
@@ -228,6 +228,7 @@ const TicketHistory = () => {
                                         <p><strong>Seat number:</strong> {seatmap[ticketId]}</p>
                                         <p><strong>Transaction Mode:</strong> {transModeMap[ticket.ticket_id]}</p>
                                         <p><strong>Route : </strong> {journey_map[transactionId].from}-{journey_map[transactionId].to}</p>
+                                        <p><strong>Price:</strong> {ticket ? ticket.price : ''}</p>
                                         <span style={{ marginLeft: '150px' }}></span>
                                     </li>
                                 );
